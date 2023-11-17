210061e5a948b773bade7d6cbac543748957b7cd
Sierra Erb <Serb2341@conestogac.on.ca>
2023-11-17 10:50:07 -0500

    Initial commit

---
+++ app/src/main/java/com/practice/saadnewpractice/MainActivity2.java
@@ -0,0 +1,46 @@
+package com.practice.saadnewpractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+
+
+    Button button8;
+    Button button9;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+
+        button8=(Button)findViewById(R.id.button8);
+
+        button8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity2.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        button9=(Button)findViewById(R.id.button9);
+
+        button9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity2.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+    }
+}
